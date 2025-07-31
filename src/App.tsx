83d30de16b3f3f516cb556e51e10b0a585a77f2b
100gun <xxxxgunshot@gmail.com>
2025-07-31 18:51:09 +0900

    modify(layout): main 태그에 공통 여백 적용
    - 메인 컨텐츠 영역에 좌우 여백 적용
    - max-width 및 margin 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-     <div className="bg-red-500 text-white p-8">
-      <h1 className="text-4xl">Tailwind 테스트</h1>
-      </div>
+      <main className="w-[1190px] max-w-[calc(100%-32px)] mx-auto">
+        <h1 className="text-4xl bg-primary text-white">Tailwind 테스트</h1>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
