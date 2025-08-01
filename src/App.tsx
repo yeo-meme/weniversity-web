38e461b19417ebe02cacdc54a0dd7d52671642ba
yeomim kim <yeomeme@gmail.com>
2025-08-01 14:34:34 +0900

    cleanup: 불필요한 코드 정리
    -헤더ui 및 공통 css 관련 코드외 정리 및 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import React from "react";
 import Header from "./components/header/header.tsx";
 import HeroSection from "./components/hero/hero-section.tsx";
 import "./index.css";
@@ -9,6 +9,7 @@ function App() {
       <Header />
       <main className="max-w-[1190px] max-[834px]:max-w-[calc(100% - 32px)] mx-auto">
         <HeroSection />
+        <div className="mt-4 flex gap-4"></div>
       </main>
     </>
   );
