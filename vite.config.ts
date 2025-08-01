1fc6020f618b5bdd696e576ec3cec1790fe6e553
yeomim kim <yeomeme@gmail.com>
2025-08-01 10:53:49 +0900

    test(config): cors 대응 테스트
    -vite.config.ts porxy설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8080,
+    proxy: {
+      "/api": {
+        target: "http://13.125.180.222",
+        changeOrigin: true,
+      },
+    },
   },
 });
