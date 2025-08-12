0b9f5bf346313a47c8f875beb795dd3efa7ab742
100gun <xxxxgunshot@gmail.com>
2025-08-12 18:37:50 +0900

    fix: 로그인 빌드 오류 해결을 위한 미사용 코드 제거(2)
    - 사용하지 않는 import 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/header/header.tsx";
 import HeroSection from "./components/hero/hero-section.tsx";
 import LoginPage from "./page/login/login.tsx";
